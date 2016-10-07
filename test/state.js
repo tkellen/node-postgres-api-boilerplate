507664ce5448ff332efc08820a72548736468e7b
Lyza Danger Gardner <lyza@lyza.com>
2016-10-07 15:17:22 -0400

    Replace test runner with mocha

--- test/state.js
+++ test/state.js
@@ -1,124 +1,178 @@
-import test from 'ava';
+/* global describe, it, before */
+
+import { expect } from 'chai';
 import request from 'supertest-as-promised';
 import { up } from './helpers/migrate';
 import fixtures from './helpers/fixtures';
 
 import app from '../src/base/app';
-import { location } from '../src/endpoints/state/routes';
+import { location} from '../src/endpoints/state/routes';
 
-test.before(t => {
+before(() => {
   up();
   return fixtures();
 });
 
-test(`GET ${location}`, t => {
-  return request(app)
-    .get(location)
-    .expect(200)
-    .then(res => {
-      t.true(res.hasOwnProperty('body'));
-      t.true(res.body.hasOwnProperty('data'));
-      t.true(Array.isArray(res.body.data));
-    });
-});
+describe (`Endpoint ${location}`, () => {
+  var firstID;
 
-test(`GET ${location}/:id`, t => {
-  return request(app)
-    .get(location)
-    .expect(200)
-    .then(res => {
-      const firstStateResource = res.body.data[0];
+  before (() => {
+    return request(app)
+      .get(location)
+      .then(res => {
+        firstID = res.body.data[0].id;
+        console.log(firstID);
+      });
+  });
+
+  describe (`GET ${location}`, () => {
+    it ('should return Array of resources', () => {
       return request(app)
-        .get(`${location}/${firstStateResource.id}`)
+        .get(location)
         .expect(200)
         .then(res => {
-          const stateResource = res.body.data;
-          ['id', 'name', 'abbr'].forEach(field => {
-            t.true(stateResource.hasOwnProperty(field));
-            t.is(firstStateResource[field], stateResource[field]);
-          });
+          expect(res).to.contain.key('body');
+          expect(res.body).to.contain.key('data');
+          expect(res.body.data).to.be.an('Array');
         });
     });
-});
+  });
 
-test(`GET ${location}/:id (invalid)`, t => {
-  const badIntRequest = request(app)
-    .get(`${location}/futz`)
-    .expect(500) // Hmmm, really?
-    .then(res => {
-      t.true(res.hasOwnProperty('error'));
-      t.truthy(res.serverError); // this seems odd
-      t.falsy(res.clientError); // as does this
+  describe (`GET ${location}/:id`, () => {
+
+    it ('should return a single resource', () => {
+      return request(app)
+        .get(`${location}/${firstID}`)
+        .expect(200)
+        .then(res => {
+          expect(res.body).to.contain.key('data');
+          expect(res.body.data).to.be.an('object');
+          expect(res.body.data).to.contain.keys('name', 'abbr', 'id');
+        });
     });
-  const nonExistentRequest = request(app)
-    .get(`${location}/1093498`)
-    .expect(404);
-  return Promise.all([badIntRequest, nonExistentRequest]);
-});
 
-test(`POST ${location} (single)`, t => {
-  const fakeState = {
-    name: 'FAKE STATE',
-    abbr: 'FS'
-  };
-  return request(app)
-    .post(location)
-    .send({
-      data: fakeState
-    })
-    .expect(201)
-    .then(res => {
-      t.truthy(res.headers.location);
-      t.truthy(res.headers.location.match(`${location}\/[0-9]+`));
-      Object.keys(fakeState).forEach(field => {
-        t.is(res.body.data[field], fakeState[field]);
+    describe(`GET ${location}/:id (invalid)`, () => {
+      it ('should handle invalid request', () => {
+        return request(app)
+          .get(`${location}/futz`)
+          .expect(500) // TODO review later
+          .then(res => {
+            expect(res).to.contain.key('error');
+            expect(res.body).to.contain.key('errors');
+            expect(res.body.errors).to.be.an('Array')
+              .and.to.have.length(1);
+            expect(res.body.errors[0]).to.be.an('object')
+              .and.to.contain.keys('code', 'message');
+            expect(res.serverError).to.be.true;
+            expect(res.clientError).to.be.false;
+          });
+      });
+      it ('should handle request for non-existent resource', () => {
+        return request(app)
+          .get(`${location}/10924899`)
+          .expect(404);
       });
     });
-});
-
-test(`POST ${location} (single, invalid)`, t => {
-  const badStates = {
-    emptyBody: {
-      data: {},
-      field: 'data.body.data' // ??? TODO later
-    },
-    missingName: {
-      data: { abbr: 'FO' },
-      field: 'data.body.data'
-    },
-    duplicateName: {
-      data: { name: 'ALABAMA'},
-      field: 'data.body.data' // This is buggy
-    },
-    invalidNameType: {
-      data: { abbr: 'FO', name: 4 },
-      field: 'data.body.data'
-    }
-  };
-  t.plan(Object.keys(badStates).length * 7);
-  const badRequests = [];
-  Object.keys(badStates).forEach(variationOnBad => {
-    badRequests.push(request(app)
-      .post(location)
-      .send({ data : badStates[variationOnBad].data })
-      .expect(409)
-      .then(res => {
-        t.truthy(res.clientError);
-        t.truthy(res.error);
-        t.falsy(res.body.data);
-        t.true(Array.isArray(res.body.errors));
-        t.true(typeof res.body.errors[0] === 'object');
-        t.is(res.body.errors[0].field, badStates[variationOnBad].field); // TODO review later
-        t.truthy(res.body.errors[0].message);
-      })
-    );
   });
-  return Promise.all(badRequests);
-});
 
-test.todo(`POST ${location} (multiple)`);
-test.todo(`POST ${location} (multiple, invalid)`);
-test.todo(`PATCH ${location}/:id`);
-test.todo(`PATCH ${location}/:id (invalid)`);
-test.todo(`DELETE ${location}/:id`);
-test.todo(`DELETE ${location}/:id (invalid)`);
+  describe ('POST ${location}', () => {
+    const fakeState = {
+      name: 'FAKE STATE',
+      abbr: 'FS'
+    };
+    it ('should create a resource', () => {
+      return request(app)
+        .post(location)
+        .send({
+          data: fakeState
+        })
+        .expect(201)
+        .then(res => {
+          expect(res.headers).to.contain.key('location');
+          expect(res.body.data).to.be.an('object')
+            .and.to.contain.keys('name', 'abbr'); // TODO automate
+        });
+    });
+    describe ('POST ${location} (invalid)', () => {
+      it ('should handle request with empty body', () => {
+        return request(app)
+          .post(location)
+          .send({})
+          .expect(409);
+      });
+      it ('should handle request with uniqueness constraint violation');
+      it ('should handle request with bad schema');
+    });
+  });
+
+  describe(`POST ${location} (multiple)`, () => {
+    const resources = [
+      { name: 'IMAGINARY', abbr: 'IM' },
+      { name: 'ILLUSORY', abbr: 'IS' }
+    ];
+    it ('should create multiple resources', () => {
+      return request(app)
+        .post(location)
+        .send({ data: resources })
+        .expect(201)
+        .then(res => {
+          expect(res.body.data).to.be.an('Array')
+            .and.to.have.length(resources.length);
+          expect(res.headers).not.to.contain.key('location');
+        });
+    });
+  });
+
+  describe(`PATCH ${location}/:id`, () => {
+    var resourceID;
+    before (() => {
+      return request(app)
+        .post(location)
+        .send({ data: {
+          name: 'FLUIDITY',
+          abbr: 'FD'
+        }})
+        .then(res => {
+          resourceID = res.body.data.id;
+          return resourceID;
+        });
+    });
+
+    it.skip ('it should patch resource', () => {
+      return request(app)
+        .patch(`${location}/${resourceID}`)
+        .send({data : { name: 'FRAGILITY'}})
+        .expect(200)
+        .then(res => {
+          console.log(res);
+        });
+    });
+  });
+
+  describe(`DELETE ${location}/:id`, () => {
+    var resourceID;
+    before (() => {
+      return request(app)
+        .post(location)
+        .send({ data: {
+          name: 'AMPHERE',
+          abbr: 'AM'
+        }})
+        .then(res => {
+          resourceID = res.body.data.id;
+          return resourceID;
+        });
+    });
+
+    it ('should delete a resource', () => {
+      return request(app)
+        .delete(`${location}/${resourceID}`)
+        .expect(204)
+        .then(res => {
+          expect(res.body).to.be.empty;
+        });
+    });
+
+    it ('should handle invalid delete requests');
+  });
+});
